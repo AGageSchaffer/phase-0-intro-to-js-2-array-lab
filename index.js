749fe580feb43e3300f34d747a285a0bb16daf37
AustinSchaffer <austinschaffer@gmail.com>
2022-12-13 08:08:18 -0600

    commit tree def33d344d7f0c1041c167a36d4a9e5bfa5aa252

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+    return cats
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+    return cats
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(-1)
+    return cats
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(0)
+    return cats
+}
+function appendCat(Broom){
+    const newCats = [...cats, Broom]
+    return newCats
+}
+function prependCat(Arnold){
+    const newCats = [Arnold, ...cats]
+    return newCats
+}
+function removeLastCat(Garfield){
+    const newCats = cats.slice(0, 2)
+    return newCats
+}
+function removeFirstCat(Milo){
+    const newCats = cats.slice(1)
+    return newCats
+}
